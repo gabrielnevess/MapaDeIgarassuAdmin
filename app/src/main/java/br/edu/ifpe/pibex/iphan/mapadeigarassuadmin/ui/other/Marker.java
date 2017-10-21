fb718d89250ce45fa0e19a33cead66cfad21edb2
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-10-21 19:07:37 -0200

    adicionando class marker

---
+++ app/src/main/java/br/edu/ifpe/pibex/iphan/mapadeigarassuadmin/ui/other/Marker.java
@@ -0,0 +1,17 @@
+package br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.ui.other;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.R;
+import br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.model.GoogleMapsModel;
+
+public class Marker {
+    public static void marker(String title, Double latitude, Double longitude){
+        GoogleMapsModel.getMap().addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .title(title)
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.marker_map)));
+    }
+}
