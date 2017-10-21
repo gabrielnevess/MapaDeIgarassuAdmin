bd29ca2482a34550d9417bc29480cc8b337934ab
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-10-21 17:43:50 -0200

    adicionando toolbar

--- app/src/main/java/br/edu/ifpe/pibex/iphan/mapadeigarassuadmin/Activity/HomeActivity.java
+++ app/src/main/java/br/edu/ifpe/pibex/iphan/mapadeigarassuadmin/Activity/HomeActivity.java
@@ -20,6 +20,7 @@ public class HomeActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
