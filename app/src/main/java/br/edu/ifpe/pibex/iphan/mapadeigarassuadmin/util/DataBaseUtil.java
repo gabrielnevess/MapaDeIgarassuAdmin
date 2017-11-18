feeb1d656b8683d22a6b5bc5c48fb29c83f26d3c
Gabriel <gabriel-silva_2011@hotmail.com>
2017-11-18 15:37:10 -0300

    atualizando class DataBaseUtil

--- app/src/main/java/br/edu/ifpe/pibex/iphan/mapadeigarassuadmin/util/DataBaseUtil.java
+++ app/src/main/java/br/edu/ifpe/pibex/iphan/mapadeigarassuadmin/util/DataBaseUtil.java
@@ -1,5 +1,6 @@
 package br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.util;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,6 +8,7 @@ import android.database.sqlite.SQLiteStatement;
 
 import br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.constants.Constants;
 import br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.model.LocationModel;
+import br.edu.ifpe.pibex.iphan.mapadeigarassuadmin.ui.other.MarkerOther;
 
 public class DataBaseUtil extends ConnectionDataBaseUtil {
 
@@ -36,20 +38,56 @@ public class DataBaseUtil extends ConnectionDataBaseUtil {
         stmt.executeInsert();
     }
 
+    public void updateLocation(LocationModel locationModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteStatement stmt = db.compileStatement(Constants.UPDATE_TABLE_LOCATION);
+        stmt.bindString(1, locationModel.getName());
+        stmt.bindDouble(2, locationModel.getLongitude());
+        stmt.bindDouble(3, locationModel.getLatitude());
+        stmt.bindString(4, locationModel.getAddress());
+        stmt.bindString(5, locationModel.getDescription());
+        stmt.bindLong(6, locationModel.getId());
+        stmt.execute();
+    }
+
+    public void deleteLocation(int locationId) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        SQLiteStatement stmt = db.compileStatement(Constants.DELETE_LOCATION);
+        stmt.bindLong(1, locationId);
+        stmt.execute();
+    }
+
     public LocationModel searchLocation(String name) {
 
-        cursor = database.query(Constants.TABLE, new String[]{Constants.ID, Constants.NAME, Constants.ADDRESS,
-                Constants.DESCRIPTION, Constants.LATITUDE, Constants.LONGITUDE}, Constants.NAME + " = \'" + name + "\' ", null, null, null, null);
+        cursor = database.query(Constants.TABLE, new String[]{Constants.NAME, Constants.ADDRESS, Constants.DESCRIPTION}, Constants.NAME + " = \'" + name + "\' ", null, null, null, null);
         cursor.moveToNext();
 
         LocationModel locationModel = new LocationModel();
-        locationModel.setId(cursor.getInt(0));
-        locationModel.setName(cursor.getString(1));
-        locationModel.setAddress(cursor.getString(2));
-        locationModel.setDescription(cursor.getString(3));
-        locationModel.setLatitude(cursor.getDouble(4));
-        locationModel.setLongitude(cursor.getDouble(5));
+        locationModel.setName(cursor.getString(0));
+        locationModel.setAddress(cursor.getString(1));
+        locationModel.setDescription(cursor.getString(2));
 
         return locationModel;
+    }
+
+    public void String(String name) {
+        cursor = database.rawQuery(Constants.SELECT_FROM_NAME, new String[]{name});
+
+    }
+
+    public void addMarkerSqlite() {
+        @SuppressLint("Recycle")
+        Cursor cursor = database.rawQuery(Constants.SELECT_ALL, null);
+
+        while (cursor.moveToNext()) {
+
+//          int id = cursor.getColumnIndex(Constants.ID);
+            int name = cursor.getColumnIndex(Constants.NAME);
+            int longitude = cursor.getColumnIndex(Constants.LONGITUDE);
+            int latitude = cursor.getColumnIndex(Constants.LATITUDE);
+
+            MarkerOther.marker(cursor.getString(name), cursor.getDouble(latitude), cursor.getDouble(longitude)); //add marker
+
+        }
     }
 }
